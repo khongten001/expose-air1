342fc0e67420e48aaf264575da998571cd4cc643
Mark <phobian@tutanota.com>
2022-01-21 07:49:59 +1100

    sync

--- look-and-feel/ExposeAir/contents/layouts/org.kde.plasma.desktop-layout.js
+++ look-and-feel/ExposeAir/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -14,16 +14,29 @@ var layout = {
                     "geometry.width": 0,
                     "geometry.x": 0,
                     "geometry.y": 0,
-                    "plugin": "org.kde.plasma.trash",
-                    "title": "Trashcan"
+                    "plugin": "org.kde.plasma.calendar",
+                    "title": "Calendar"
                 },
                 {
                     "config": {
                         "/": {
-                            "PreloadWeight": "31"
+                            "PreloadWeight": "0"
+                        }
+                    },
+                    "geometry.height": 0,
+                    "geometry.width": 0,
+                    "geometry.x": 0,
+                    "geometry.y": 0,
+                    "plugin": "org.kde.plasma.mediacontroller",
+                    "title": "Media Player"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "PreloadWeight": "42"
                         },
                         "/ConfigDialog": {
-                            "DialogHeight": "496",
+                            "DialogHeight": "480",
                             "DialogWidth": "640"
                         },
                         "/General": {
@@ -40,29 +53,31 @@ var layout = {
             ],
             "config": {
                 "/": {
-                    "ItemGeometries-1920x1080": "Applet-266:1712,32,176,160,0;Applet-255:1760,864,96,96,0;",
-                    "ItemGeometriesHorizontal": "Applet-266:1712,32,176,160,0;Applet-255:1760,864,96,96,0;",
+                    "ItemGeometries-1024x768": "Applet-41:1296,688,592,96,0;",
+                    "ItemGeometries-1920x1080": "Applet-117:1520,240,336,336,0;Applet-114:1520,576,336,336,0;Applet-130:1568,32,240,192,0;",
+                    "ItemGeometries-800x600": "",
+                    "ItemGeometriesHorizontal": "Applet-117:1520,240,336,336,0;Applet-114:1520,576,336,336,0;Applet-130:1568,32,240,192,0;",
                     "formfactor": "0",
                     "immutability": "1",
                     "lastScreen": "0",
                     "wallpaperplugin": "org.kde.image"
                 },
                 "/ConfigDialog": {
-                    "DialogHeight": "791",
-                    "DialogWidth": "1177"
+                    "DialogHeight": "678",
+                    "DialogWidth": "938"
                 },
                 "/Configuration": {
                     "PreloadWeight": "0"
                 },
                 "/General": {
                     "ToolBoxButtonState": "topcenter",
-                    "ToolBoxButtonX": "624"
+                    "ToolBoxButtonX": "606"
                 },
                 "/Wallpaper/org.kde.color/General": {
-                    "Color": "202,202,0"
+                    "Color": "67,149,209"
                 },
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "file:///home/mark/Public/Expose Modern/exmod.png",
+                    "Image": "file:///home/mark/Walls/win7logolessbright.png",
                     "SlidePaths": "/usr/share/wallpapers"
                 }
             },
@@ -79,10 +94,13 @@ var layout = {
                             "immutability": "1"
                         },
                         "/Configuration": {
-                            "PreloadWeight": "10"
+                            "PreloadWeight": "36"
                         },
                         "/Configuration/General": {
                             "favoritesPortedToKAstats": "true"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
                         }
                     },
                     "plugin": "org.kde.plasma.kickoff"
@@ -95,43 +113,11 @@ var layout = {
                         "/Configuration": {
                             "PreloadWeight": "0"
                         },
-                        "/Configuration/ConfigDialog": {
-                            "DialogHeight": "496",
-                            "DialogWidth": "640"
-                        },
                         "/Configuration/General": {
-                            "launchers": "preferred://browser,preferred://filemanager,applications:org.kde.konsole.desktop,applications:systemsettings.desktop,applications:org.kde.gwenview.desktop,applications:cantata.desktop",
-                            "onlyGroupWhenFull": "false"
+                            "launchers": "applications:chromium.desktop,applications:systemsettings.desktop,preferred://filemanager,applications:org.kde.gwenview.desktop,applications:org.kde.konsole.desktop,applications:vscode.desktop,applications:org.kde.ktorrent.desktop"
                         }
                     },
-                    "plugin": "org.kde.plasma.taskmanager"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration/ConfigDialog": {
-                            "DialogHeight": "496",
-                            "DialogWidth": "640"
-                        },
-                        "/Configuration/General": {
-                            "maximumWidth": "330",
-                            "preferredSource": "cantata"
-                        }
-                    },
-                    "plugin": "org.kde.mediabar"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration": {
-                            "PreloadWeight": "0"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.pager"
+                    "plugin": "org.kde.plasma.icontasks"
                 },
                 {
                     "config": {
@@ -153,12 +139,11 @@ var layout = {
                             "PreloadWeight": "0"
                         },
                         "/Configuration/Appearance": {
-                            "fontFamily": "Selawik Semilight",
-                            "showDate": "false",
+                            "fontFamily": "Victor Mono",
                             "use24hFormat": "2"
                         },
                         "/Configuration/ConfigDialog": {
-                            "DialogHeight": "578",
+                            "DialogHeight": "540",
                             "DialogWidth": "720"
                         }
                     },
@@ -173,11 +158,12 @@ var layout = {
                             "PreloadWeight": "0"
                         },
                         "/Configuration/ConfigDialog": {
-                            "DialogHeight": "480",
-                            "DialogWidth": "640"
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
                         },
                         "/Configuration/General": {
-                            "show_lockScreen": "false"
+                            "show_requestLogout": "true",
+                            "show_requestShutDown": "false"
                         }
                     },
                     "plugin": "org.kde.plasma.lock_logout"
@@ -198,7 +184,7 @@ var layout = {
                     "PreloadWeight": "0"
                 }
             },
-            "height": 2.25,
+            "height": 2.5,
             "hiding": "normal",
             "location": "bottom",
             "maximumLength": 120,
