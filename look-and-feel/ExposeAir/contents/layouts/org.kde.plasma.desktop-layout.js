9aa7c32826585a26589df01897643f96e0fe1189
Mark <phobian@tutanota.com>
2023-05-27 08:57:20 +1000

    sync

--- look-and-feel/ExposeAir/contents/layouts/org.kde.plasma.desktop-layout.js
+++ look-and-feel/ExposeAir/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,195 +1,69 @@
-var plasma = getApiVersion(1);
+var panel = new Panel
+var panelScreen = panel.screen
 
-var layout = {
-    "desktops": [
-        {
-            "applets": [
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0"
-                        },
-                        "/ConfigDialog": {
-                            "DialogHeight": "510",
-                            "DialogWidth": "680"
-                        },
-                        "/General": {
-                            "showSecondHand": "true"
-                        }
-                    },
-                    "geometry.height": 0,
-                    "geometry.width": 0,
-                    "geometry.x": 0,
-                    "geometry.y": 0,
-                    "plugin": "org.kde.plasma.analogclock",
-                    "title": "Analog Clock"
-                }
-            ],
-            "config": {
-                "/": {
-                    "ItemGeometries-1920x1080": "Applet-149:1696,16,208,192,0;Applet-155:1168,224,752,576,0;Applet-157:1440,832,464,160,0;",
-                    "ItemGeometries-2560x1331": "",
-                    "ItemGeometries-2560x1440": "Applet-149:2320,48,192,192,0;",
-                    "ItemGeometriesHorizontal": "Applet-149:2320,48,192,192,0;",
-                    "formfactor": "0",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                },
-                "/ConfigDialog": {
-                    "DialogHeight": "570",
-                    "DialogWidth": "760"
-                },
-                "/Configuration": {
-                    "PreloadWeight": "0"
-                },
-                "/General": {
-                    "ToolBoxButtonState": "topcenter",
-                    "ToolBoxButtonX": "927",
-                    "positions": "{\"1920x1080\":[]\\,\"2560x1440\":[]}",
-                    "sortMode": "-1"
-                },
-                "/Wallpaper/org.kde.color/General": {
-                    "Color": "85,170,255"
-                },
-                "/Wallpaper/org.kde.image/General": {
-                    "Image": "/home/mark/.local/share/wallpapers/ExposeAir/",
-                    "SlidePaths": "/usr/share/wallpapers"
-                }
-            },
-            "wallpaperPlugin": "org.kde.image"
-        }
-    ],
-    "panels": [
-        {
-            "alignment": "center",
-            "applets": [
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "85"
-                        },
-                        "/General": {
-                            "favoritesPortedToKAstats": "true"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.kickoff"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.marginsseparator"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0",
-                            "localPath": "/home/mark/.local/share/plasma_icons/microsoft-edge.desktop",
-                            "url": "file:///usr/share/applications/microsoft-edge.desktop"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.icon"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0",
-                            "localPath": "/home/mark/.local/share/plasma_icons/org.kde.dolphin.desktop",
-                            "url": "file:///usr/share/applications/org.kde.dolphin.desktop"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.icon"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0",
-                            "localPath": "/home/mark/.local/share/plasma_icons/org.kde.konsole.desktop",
-                            "url": "file:///usr/share/applications/org.kde.konsole.desktop"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.icon"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0"
-                        },
-                        "/General": {
-                            "launchers": ""
-                        }
-                    },
-                    "plugin": "org.kde.plasma.icontasks"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.pager"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.systemtray"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "0"
-                        },
-                        "/Appearance": {
-                            "fontFamily": "Selawik",
-                            "showDate": "false"
-                        },
-                        "/ConfigDialog": {
-                            "DialogHeight": "570",
-                            "DialogWidth": "760"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.digitalclock"
-                },
-                {
-                    "config": {
-                        "/": {
-                            "PreloadWeight": "34"
-                        }
-                    },
-                    "plugin": "org.kde.plasma.lock_logout"
-                }
-            ],
-            "config": {
-                "/": {
-                    "formfactor": "2",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                },
-                "/ConfigDialog": {
-                    "DialogHeight": "84",
-                    "DialogWidth": "2560"
-                },
-                "/Configuration": {
-                    "PreloadWeight": "0"
-                }
-            },
-            "height": 2.6666666666666665,
-            "hiding": "normal",
-            "location": "bottom",
-            "maximumLength": 142.22222222222223,
-            "minimumLength": 142.22222222222223,
-            "offset": 0
-        }
-    ],
-    "serializationFormatVersion": "1"
+panel.location = "botom";
+panel.height = 44
+const geo = screenGeometry(panelScreen);
+panel.alignment = "center";
+panel.minimumLength = geo.width;
+panel.maximumLength = geo.width;
+
+var kickoff = panel.addWidget("org.kde.plasma.kickoff")
+kickoff.currentConfigGroup = ["Shortcuts"]
+kickoff.writeConfig("global", "Alt+F1")
+kickoff.currentConfigGroup = ["General"]
+kickoff.writeConfig("favorites", ["preferred://browser", "org.kde.dolphin.desktop", "org.kde.konsole.desktop", "systemsettings.desktop"])
+
+//panel.addWidget("org.kde.plasma.showActivityManager")
+let taskBar = panel.addWidget("org.kde.plasma.icontasks")
+taskBar.currentConfigGroup = ["General"]
+taskBar.writeConfig("launchers",["preferred://browser", "preferred://filemanager","applications:org.kde.konsole.desktop"])
+panel.addWidget("org.kde.plasma.marginsseparator")
+panel.addWidget("org.kde.plasma.pager")
+
+/* Next up is determining whether to add the Input Method Panel
+ * widget to the panel or not. This is done based on whether
+ * the system locale's language id is a member of the following
+ * white list of languages which are known to pull in one of
+ * our supported IME backends when chosen during installation
+ * of common distributions. */
+
+var langIds = ["as",    // Assamese
+               "bn",    // Bengali
+               "bo",    // Tibetan
+               "brx",   // Bodo
+               "doi",   // Dogri
+               "gu",    // Gujarati
+               "hi",    // Hindi
+               "ja",    // Japanese
+               "kn",    // Kannada
+               "ko",    // Korean
+               "kok",   // Konkani
+               "ks",    // Kashmiri
+               "lep",   // Lepcha
+               "mai",   // Maithili
+               "ml",    // Malayalam
+               "mni",   // Manipuri
+               "mr",    // Marathi
+               "ne",    // Nepali
+               "or",    // Odia
+               "pa",    // Punjabi
+               "sa",    // Sanskrit
+               "sat",   // Santali
+               "sd",    // Sindhi
+               "si",    // Sinhala
+               "ta",    // Tamil
+               "te",    // Telugu
+               "th",    // Thai
+               "ur",    // Urdu
+               "vi",    // Vietnamese
+               "zh_CN", // Simplified Chinese
+               "zh_TW"] // Traditional Chinese
+
+if (langIds.indexOf(languageId) != -1) {
+    panel.addWidget("org.kde.plasma.kimpanel");
 }
-;
 
-plasma.loadSerializedLayout(layout);
+panel.addWidget("org.kde.plasma.systemtray")
+panel.addWidget("org.kde.plasma.digitalclock")
+
